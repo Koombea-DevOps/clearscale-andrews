50c340c19e6e8f127203429e5bddeedf8c50fa27
AndrewsHerrera <andrews.herrera@koombea.com>
2019-11-25 01:49:16 -0500

    Add ECS permissions to the EC2 instance role

--- terraform-code/iam.tf
+++ terraform-code/iam.tf
@@ -53,7 +53,7 @@ EOF
 
 resource "aws_iam_policy_attachment" "ecs_service_policy_attachment" {
   name       = "${terraform.workspace}-${var.project_name}-instance-policy-attachment"
-  roles      = ["${aws_iam_role.ecs_service_role.name}"]
+  roles      = ["${aws_iam_role.ec2_instance_role.name}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
